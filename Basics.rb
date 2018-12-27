4d504783c1c33d7b7b036dedcab2582089d1273a
Saber <saberkhoshdel@users.noreply.github.com>
2018-12-27 00:22:53 -0500

    Update Basics.rb

--- Basics.rb
+++ Basics.rb
@@ -1,12 +1,10 @@
 # learning Ruby
 # Download and install latest Ruby + development kit
-# comment with #, no need for any signs at the end of line
+# comment with #, no need for any signs at the end of lines
+# First command: Put String (puts) on screen
 puts "Hello World!"
 
 # Difference between print and put string (puts) is new line
-puts "Hello World!"
-puts "My name is Saber"
-puts ""
 print "Hello World!"
 puts "My name is Saber"
 
@@ -16,4 +14,27 @@ system "clear"
 
 # Ruby is an object-oriented language
 # Object Manipulation achieved with dot after object
-puts "My name is Saber".upcase
+puts ""
+puts "Hello World!".upcase
+puts "Hello World!".downcase
+puts "Hello World!".capitalize
+puts "Hello World!".reverse
+puts "Hello World!".length
+# If an object is number, we can do numbery things
+# If it is a string, we can do stringy things
+
+# Variables
+# Startes with lower case. Upper case ones are for classes
+# No need for defining varilable type beforehand
+first_name = "Saber"
+puts first_name
+age = 28
+
+# Getting user input using gets
+#print "Enter Your Name: "
+#name = gets
+#puts "Hello " + name
+# Getting user input using gets.chomp, with no new line
+print "Enter Your Name: "
+name = gets.chomp
+puts "Hello #{ name.upcase }, how are you?"
