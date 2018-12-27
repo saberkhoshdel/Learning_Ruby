c66f32719738f81fbd4803654ff045ca2ed43cc8
Saber <saberkhoshdel@users.noreply.github.com>
2018-12-26 23:53:51 -0500

    Create Basics.rb

---
+++ Basics.rb
@@ -0,0 +1,19 @@
+# learning Ruby
+# Download and install latest Ruby + development kit
+# comment with #, no need for any signs at the end of line
+puts "Hello World!"
+
+# Difference between print and put string (puts) is new line
+puts "Hello World!"
+puts "My name is Saber"
+puts ""
+print "Hello World!"
+puts "My name is Saber"
+
+# Clearing screen,
+system "clear"
+# seems not working in Windows command prompt
+
+# Ruby is an object-oriented language
+# Object Manipulation achieved with dot after object
+puts "My name is Saber".upcase
